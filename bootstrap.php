a619543fa31db727e711de56dab11a1d3d1b5170
Puleeno Nguyen <puleeno@gmail.com>
2024-05-12 19:42:28 +0700

    Create function jankx_is_support_block_template

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 
 add_theme_support('block-templates');
+
+function jankx_is_support_block_template()
+{
+    return apply_filters(
+        'jankx/gutenberg/enabled',
+        get_theme_support('block-templates')
+    );
+}
--- src/Gutenberg.php
+++ src/Gutenberg.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace Jankx\Gutenberg;
 
-class Gutenberg {
-    public function init() {
+class Gutenberg
+{
+    public function init()
+    {
         // add_filter('pre_get_block_file_template', [$this, 'changeTemplatesPaths'])
     }
 }
