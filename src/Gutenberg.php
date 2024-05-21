f3cae28451130363e7421ce23cb44e36aee18dc7
Puleeno <puleeno@gmail.com>
2024-05-21 19:48:24 +0700

    Create singleton pattern

--- src/Gutenberg.php
+++ src/Gutenberg.php
@@ -4,6 +4,18 @@ namespace Jankx\Gutenberg;
 
 class Gutenberg
 {
+    protected static $instance;
+
+    protected function __construct() {
+    }
+
+    public static function getInstance() {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
     public function init()
     {
         // add_filter('pre_get_block_file_template', [$this, 'changeTemplatesPaths'])
