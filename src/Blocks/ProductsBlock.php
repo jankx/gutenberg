921223152ca8c3519104de896a351a42e9c0dcd0
Puleeno Nguyen <puleeno@gmail.com>
2024-06-27 22:27:43 +0700

    Change block version and render products block content

--- src/Blocks/ProductsBlock.php
+++ src/Blocks/ProductsBlock.php
@@ -3,6 +3,8 @@
 namespace Jankx\Gutenberg\Blocks;
 
 use Jankx\Blocks\BlockAbstract;
+use Jankx\Ecommerce\Base\Renderer\ProductsRenderer;
+use Jankx\PostLayout\Layout\Card;
 use WooCommerce;
 
 class ProductsBlock extends BlockAbstract
@@ -11,10 +13,41 @@ class ProductsBlock extends BlockAbstract
 
     public function isEnabled(): bool
     {
-        return class_exists(WooCommerce::class);
+        return class_exists(WooCommerce::class) && defined('JANKX_ECOMMERCE_FILE_LOADER');
     }
 
     public function render()
     {
+        $settings = [];
+        $productsModule = new ProductsRenderer(array(
+            'widget_title' => array_get($settings, 'title', 10),
+            'categories' => array_get($settings, 'product_categories', array()),
+            'tags' => array_get($settings, 'product_tags', array()),
+            'layout' => $this->get_responsive_setting('layout', Card::LAYOUT_NAME),
+            'limit' => $this->get_responsive_setting('limit', 10),
+        ));
+        if (($url = array_get($settings, 'readmore_url', ''))) {
+            $productsModule->setReadMore($url);
+        }
+
+        $productsModule->setLayoutOptions(array(
+            'columns_tablet' => array_get($settings, 'columns_tablet', 2),
+            'columns_mobile' => array_get($settings, 'columns', 1),
+            'columns' => $this->get_responsive_setting('columns', 4),
+            'rows' => $this->get_responsive_setting('rows', 1),
+            'thumbnail_size'  => array_get($settings, 'thumbnail_size', 'medium'),
+            'wrap_tag_name' => 'ul',
+        ));
+
+        // Set Woocommerce loop columns
+        wc_get_loop_prop('columns', $this->get_responsive_setting('columns', 4));
+
+        // Render the content
+        $productsContent = '';
+        $widgetTitle = array_get($settings, 'title');
+        if ($widgetTitle && $productsContent) {
+            $productsContent .= sprintf('<h3 class="products-widget-title"><span>%s</span></h3>', $widgetTitle);
+        }
+        return $productsContent . $productsModule->render(false);
     }
 }
