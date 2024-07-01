0246170beb96821ecf87038313cdbb30351eaf45
Puleeno Nguyen <puleeno@gmail.com>
2024-07-02 00:13:48 +0700

    Remove all Jankx based common on Gutenberg page

--- src/Gutenberg.php
+++ src/Gutenberg.php
@@ -56,26 +56,6 @@ class Gutenberg
         return dirname(JANKX_GUTENBERG_BOOT_FILE);
     }
 
-    public function createCoreGroupWrapper($parsed_block, $parent_block)
-    {
-        if ($parsed_block['blockName'] === 'core/group' && $parent_block == $parsed_block) {
-            ob_start();
-            jankx_open_container();
-            $container = ob_get_clean();
-            array_unshift($parsed_block['innerContent'], $container);
-            array_unshift($parsed_block['innerContent'], sprintf('<div %s>', jankx_generate_html_attributes([
-                'class' => 'jankx-base'
-            ])));
-
-            ob_start();
-            jankx_close_container();
-            $closeContainer = ob_get_clean();
-            array_push($parsed_block['innerContent'], '</div>');
-            array_push($parsed_block['innerContent'], $closeContainer);
-        }
-        return $parsed_block;
-    }
-
     public function boot()
     {
         add_filter('pre_get_block_templates', [$this, 'changeTemplatesPaths'], 9999, 3);
@@ -88,26 +68,6 @@ class Gutenberg
         add_action('jankx/template/render/start', function ($page) {
             if ($page->isGutenbergSupport() && (in_array($page->getLoadedLayout(), [SiteLayout::LAYOUT_FULL_WIDTH]))) {
                 add_filter('jankx/layout/based/common-css', '__return_false');
-
-                add_action('jankx_component_before_header_content', [$this, 'openWrapper']);
-                add_action('jankx_component_after_header_content', [$this, 'closeWrapper']);
-
-                add_action('jankx/template/footer/before_content', [$this, 'openWrapper']);
-                add_action('jankx/template/footer/after_content', [$this, 'closeWrapper']);
-
-                add_filter('jankx_template_footer_widget_wrapper_class', function ($classes) {
-                    $classes[] = 'jankx-base';
-
-                    return $classes;
-                });
-
-                add_action('jankx/template/page/content/before', function () {
-                    add_filter('render_block_data', [$this, 'createCoreGroupWrapper'], 10, 2);
-                });
-
-                add_action('jankx/template/page/content/after', function () {
-                    remove_filter('render_block_data', [$this, 'createCoreGroupWrapper'], 10);
-                });
             }
         });
     }
