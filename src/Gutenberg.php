2b1d7c0b726dff7640430d18b08976471584920b
Puleeno Nguyen <puleeno@gmail.com>
2025-07-20 18:15:44 +0700

    fix call Jankx without namespace

--- src/Gutenberg.php
+++ src/Gutenberg.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) {
     exit('Cheating huh?');
 }
 
-use Jankx;
+use Jankx\Jankx;
 use Jankx\Blocks\BlockInterface;
 use Jankx\Blocks\GutenbergFilterInterface;
 use Jankx\Gutenberg\Blocks\ContactForm7Block;
